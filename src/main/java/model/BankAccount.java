7bc80ad1d2ed4f6a04a8b9f52dafc24e40d17de5
Ruggery <ruggeryalves@hotmail.com>
2025-07-09 14:08:07 +0100

    Starting Bank System project with MySQL and JSP.

---
+++ src/main/java/model/BankAccount.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Ruggery
+ */
+public class BankAccount {
+    
+    private int id;
+    private String username;
+    private int accountNumber;
+    private double balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    
+    
+}
